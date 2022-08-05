a172437e7332d8753e755803a7c931c4ae151b4d
hanasiu <102237333+hanasiu@users.noreply.github.com>
2022-08-05 13:11:45 +0900

    Create Chapter04_ex06.cpp

---
+++ Chapter4/Chapter04_ex06.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+   vector < string > numList = {
+      "zero",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine"
+   };
+   int num = 0;
+   string numString = "";
+   while (cin >> num >> numString) {
+      cout << numList[num] << endl;
+      for (int i = 0; i < numList.size(); ++i) {
+         if (numList[i] == numString) {
+            cout << i << endl;
+         }
+      }
+   }
+
+   return 0;
+}
