b8cf8c7079817edf89e8f1b4babfbd880acfbd1a
hanasiu <102237333+hanasiu@users.noreply.github.com>
2022-09-15 14:00:25 +0900

    Create Chapter06_ex05.cpp

---
+++ Chapter6/Chapter06_ex05.cpp
@@ -0,0 +1,24 @@
+/*
+Sentence: 
+   Noun Verb
+   Article Noun Verb
+   Sentence Conjunction Sentence
+   
+Conjunction:
+   "and"
+   "or"
+   "but"
+   
+Noun:
+   "birds"
+   "fish"
+   "C++"
+   
+Verb:
+   "rules"
+   "fly"
+   "swim"
+   
+Article:
+   "the"
+*/
