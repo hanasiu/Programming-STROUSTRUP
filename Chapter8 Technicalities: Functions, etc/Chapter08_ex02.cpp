798a690b2021dde1d6b9253780c843b32f576973
hanasiu <102237333+hanasiu@users.noreply.github.com>
2022-10-14 15:16:57 +0900

    Update Chapter08_ex02.cpp

--- Chapter8 Technicalities: Functions, etc/Chapter08_ex02.cpp
+++ Chapter8 Technicalities: Functions, etc/Chapter08_ex02.cpp
@@ -1,12 +1,19 @@
 #include "std_lib_facilities.h"
 
+using namespace std;
+
 void print(string label, const vector<int> vec) {
 	cout << label << " (" << vec.size() << "): {";
 	for (int i = 0; i < vec.size(); ++i) {
+		if (i % 10 == 0) cout << endl;
 		cout << vec[i] ;
 		if (i < vec.size() - 1) cout << ", ";
 	}
 	cout << "}\n";
+}
+
+bool findMaxInt(int x, int y) {
+	return x > y;
 }
 
 void fibonacci(int x, int y, vector<int>& vec, int num) {
@@ -17,10 +24,16 @@ void fibonacci(int x, int y, vector<int>& vec, int num) {
 	vec.emplace_back(y);
 	for (int i = 2; i < num; ++i) {
 		vec.emplace_back(vec[i - 2] + vec[i - 1]);
+		if (findMaxInt(vec[i - 2], vec[i - 1])) {
+			cout << "Max Int is " << vec[i - 2] << endl;
+			vec.erase(vec.end() - 2, vec.end());
+			break;
+		}
 	}
 }
 
-int main() {
+int main() 
+try {
 	vector<int> instance = {};
 	int first = 1;
 	int second = 2;
@@ -30,7 +43,18 @@ int main() {
 			<< "first and second number, and how many fibonacci numbers do you want?" << endl;
 		cin >> first >> second >> num;
 		fibonacci(first, second, instance, num);
-		print("fibonacci", instance);
+		print("Fibonacci List", instance);
 	}
-	return 0;
+}
+catch (exception& e) {
+	cerr << "exception: " << e.what() << endl;
+	char c;
+	while (cin >> c && c != ';');
+	return 1;
+}
+catch (...) {
+	cerr << "exception\n";
+	char c;
+	while (cin >> c && c != ';');
+	return 2;
 }
