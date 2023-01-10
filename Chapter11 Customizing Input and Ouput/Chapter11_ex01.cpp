8104abee31043b174ea45e8bf947af1614feeef0
hanasiu <102237333+hanasiu@users.noreply.github.com>
2023-01-10 11:46:24 +0900

    Create Chapter11_ex01.cpp

---
+++ Chapter11 Customizing Input and Ouput/Chapter11_ex01.cpp
@@ -0,0 +1,29 @@
+#include "std_lib_facilities.h"
+
+
+int main()
+try {
+    cout << "Enter input file name: ";
+    string iname;
+    cin >> iname;
+    ifstream ifs(iname.c_str());
+    if (!ifs) error("can't open input file", iname);
+
+    cout << "Enter output file name: ";
+    string oname;
+    cin >> oname;
+    ofstream ofs(oname.c_str());
+    if (!ofs) error("can't open output file", oname);
+
+    char ch;
+    while (ifs.get(ch)) {
+        if (isalpha(ch)) ch = tolower(ch);
+        ofs << ch;
+    }
+}
+catch (exception& e) {
+    cerr << "exception: " << e.what() << endl;
+}
+catch (...) {
+    cerr << "exception\n";
+}
