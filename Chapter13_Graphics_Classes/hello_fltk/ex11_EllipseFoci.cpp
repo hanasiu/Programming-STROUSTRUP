d1a8b145979877e44059b100376a77ef9457b314
kimyu <kimyu@hanasiu.local>
2023-06-10 14:18:58 +0900

    13_11

--- Chapter13_Graphics_Classes/hello_fltk/ex11_EllipseFoci.cpp
+++ Chapter13_Graphics_Classes/hello_fltk/ex11_EllipseFoci.cpp
@@ -84,6 +84,36 @@ void example(Simple_window& win) {
     Text W {Point{w(ellipse)}, "w: " + to_string(w(ellipse).x) + ", " + to_string(w(ellipse).y)};
     win.attach(W);
 
+    Line horizontal{Point{200,300},Point{600,300}};
+    win.attach(horizontal);
+    Line vertical{Point{400,150},Point{400,450}};
+    win.attach(vertical);
+// Calculate the foci
+    double a = ellipse.major()/2.0;
+    double b = ellipse.minor()/2.0;
+    double e = std::sqrt(1.0 - (b * b) / (a * a));
+    Point focus1{ellipse.center().x + static_cast<int>(a * e), ellipse.center().y};
+    Point focus2{ellipse.center().x - static_cast<int>(a * e), ellipse.center().y};
+
+    // Display the foci
+    Circle foci1{focus1, 3};  // Adjust the size of the circle as needed
+    foci1.set_fill_color(Color::red);  // Set the fill color of the circle
+    win.attach(foci1);
+
+    Circle foci2{focus2, 3};  // Adjust the size of the circle as needed
+    foci2.set_fill_color(Color::red);  // Set the fill color of the circle
+    win.attach(foci2);
+
+    // Display the coordinates of the foci
+    Text F1{Point{focus1}, to_string(Point{focus1}.x) + ", " + to_string(Point{focus1}.y)};
+    F1.set_color(Color::red);  // Set the color of the text
+    win.attach(F1);
+
+    Text F2{Point{focus2}, to_string(Point{focus2}.x) + ", " + to_string(Point{focus2}.y)};
+    F2.set_color(Color::red);  // Set the color of the text
+    win.attach(F2);
+    
+
     win.set_label("Canvas #1");
     win.wait_for_button();
 }
@@ -94,4 +124,4 @@ int main() {
     example(win);
 }
 
-//g++ -w -Wall -std=c++11 Graph.cpp Window.cpp GUI.cpp Simple_window.cpp ex05_Ellipse.cpp -I/opt/homebrew/Cellar/fltk/1.3.8_1/include -L/opt/homebrew/Cellar/fltk/1.3.8_1/lib -L/opt/homebrew/opt/libpng/lib -L/opt/homebrew/opt/jpeg-turbo/lib `fltk-config --cxxflags --ldflags --use-images` -o hello_fltk -lpng -lturbojpeg && ./hello_fltk
+//g++ -w -Wall -std=c++11 Graph.cpp Window.cpp GUI.cpp Simple_window.cpp ex11_EllipseFoci.cpp -I/opt/homebrew/Cellar/fltk/1.3.8_1/include -L/opt/homebrew/Cellar/fltk/1.3.8_1/lib -L/opt/homebrew/opt/libpng/lib -L/opt/homebrew/opt/jpeg-turbo/lib `fltk-config --cxxflags --ldflags --use-images` -o hello_fltk -lpng -lturbojpeg && ./hello_fltk
